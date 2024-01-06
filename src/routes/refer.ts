367299cef0e54daee0c31b59122b5dc58e9017db
Sirshak Bohara <boharasirshak101@gmail.com>
2024-01-06 20:59:11 +0545

    Add error handling and redirect functionality to refer route

--- src/routes/refer.ts
+++ src/routes/refer.ts
@@ -1,11 +1,23 @@
 import express, { Router, Request, Response } from 'express';
+import { Links } from '../lib/db';
 
 const router: Router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-    let code = req.query.code;
-    // check for code in a database
-    // get the redirect url
+router.get('/', async (req: Request, res: Response) => {
+    let code: any = req.query.code;
+    if (Number.isNaN(code)) {
+        return res.status(400).json({ error: "Parameter code is not an integer" });
+    }
+    let link: Links | null = await Links.findOne({
+        where: {
+            createdBy: code
+        }
+    });
+    if (link === null) {
+        return res.status(404).json({ error: "Invalid refer code!" });
+    }
+    await link.increment('views');
+    res.redirect(link.redirectUrl);
 });
 
 export { router };
