c86d5a793c947f5ed5124534b1e73de843492dfa
Sirshak Bohara <boharasirshak101@gmail.com>
2024-01-06 14:33:49 +0545

    Add express server and routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('hello world');
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import {router as referRouter} from './routes/refer';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
+app.use('/refer', referRouter);
+
+
+// Define routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
